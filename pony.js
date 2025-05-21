6c2791ecdc5fe088028a509f202a2ca7a001d327
webreflection <andrea.giammarchi@gmail.com>
2025-05-21 11:10:26 +0200

    When in Rome ...

--- pony.js
+++ pony.js
@@ -1,36 +1,39 @@
 /**
- * @param {ProxyConstructor} Proxy
+ * @param {ProxyConstructor} Proxy the proxy constructor to patch.
+ * @param {Symbol} [target] a "secret" to retrieve a callback that reveals proxies' targets.
  * @returns {ProxyConstructor & { isProxy: (ref:object) => boolean }}
  */
-export default Proxy => {
+export default (Proxy, target = Symbol.for('is-proxy')) => {
+  const { apply, construct, has, get, ownKeys } = Reflect;
+  const { revocable } = Proxy;
   const isProxy = 'isProxy';
-  if (!(isProxy in Proxy)) {
-    const { apply, construct, has, get, ownKeys } = Reflect;
-    const { revocable } = Proxy;
-    const named = {
-      revocable(...args) {
-        const target = apply(revocable, this, args);
-        addProxy(target.proxy);
-        return target;
-      }
-    };
-    const proxies = new WeakSet;
-    const addProxy = proxies.add.bind(proxies);
-    const hasProxy = proxies.has.bind(proxies);
-    Proxy = new Proxy(Proxy, {
-      construct: (Proxy, args, New) => {
-        const target = construct(Proxy, args, New);
-        addProxy(target);
-        return target;
-      },
-      has: (Proxy, key) => key === isProxy || has(Proxy, key),
-      get: (Proxy, key, receiver) => {
-        if (key === isProxy) return hasProxy;
-        if (key === 'revocable') return named.revocable;
-        return get(Proxy, key, receiver);
-      },
-      ownKeys: Proxy => [isProxy, ...ownKeys(Proxy)],
-    });
-  }
-  return Proxy;
+  const proxies = new WeakMap;
+  const getProxy = proxies.get.bind(proxies);
+  const hasProxy = proxies.has.bind(proxies);
+  const setProxy = proxies.set.bind(proxies);
+  const named = {
+    revocable(...args) {
+      const target = apply(revocable, this, args);
+      setProxy(target.proxy, args[0]);
+      return target;
+    }
+  };
+  return isProxy in Proxy ? Proxy : new Proxy(Proxy, {
+    construct: (Proxy, args, New) => {
+      const proxy = construct(Proxy, args, New);
+      setProxy(proxy, args[0]);
+      return proxy;
+    },
+    has: (Proxy, key) => key === isProxy || has(Proxy, key),
+    get: (Proxy, key, receiver) => {
+      if (key === isProxy) return hasProxy;
+      if (key === 'revocable') return named.revocable;
+      return key === target ? getProxy : get(Proxy, key, receiver);
+    },
+    ownKeys: Proxy => {
+      const keys = ownKeys(Proxy);
+      keys.push(isProxy);
+      return keys;
+    },
+  });
 };
